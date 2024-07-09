cca7b3be1ca489d178789c5fa05a1f3c8e6a01d7
cilasmarques <cilas@copin.ufcg.edu.br>
2024-07-09 20:01:01 -0300

    feat: add tiffs reader

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 build-serial:
-	/usr/local/cuda/bin/nvcc -g ./src/serial.cu -o ./serial
+	/usr/local/cuda/bin/nvcc -g ./src/serial.cu -o ./serial -ltiff -std=c++14
 
 build-cuda-core:
-	/usr/local/cuda/bin/nvcc -g ./src/cudaCores.cu -o ./cudaCores
+	/usr/local/cuda/bin/nvcc -g ./src/cudaCores.cu -o ./cudaCores -ltiff -std=c++14
 
 build-cutensor:
-	/usr/local/cuda/bin/nvcc -g ./src/cudaTensor.cu -o ./cuTensor -lcutensor -lcublas 
+	/usr/local/cuda/bin/nvcc -g ./src/cudaTensor.cu -o ./cuTensor -lcutensor -lcublas -ltiff -std=c++14
 
 build-cublas:
-	/usr/local/cuda/bin/nvcc -g ./src/cublasTensor.cu -o ./cublas -lcutensor  -lcublas 
+	/usr/local/cuda/bin/nvcc -g ./src/cublasTensor.cu -o ./cublas -lcutensor -lcublas -ltiff -std=c++14
 
 execute:
 	./main
--- src/serial.cu
+++ src/serial.cu
@@ -2,89 +2,101 @@
 #include <chrono>
 #include <vector>
 #include <thread>
+#include <tiffio.h>
+#include <string>
 
 using namespace std::chrono;
 
 // Define the matrix size
-#define MATRIX_HEIGTH 128*64
-#define MATRIX_WIDTH 128*64
-
-void multiplyPartial(float *matrixA, float *matrixB, float *matrixC, int startRow, int endRow)
-{
-  for (int row = startRow; row < endRow; ++row)
-  {
-    for (int col = 0; col < MATRIX_WIDTH; ++col)
-    {
-      for (int i = 0; i < MATRIX_WIDTH; ++i)
-      {
-        matrixC[row * MATRIX_WIDTH + col] += matrixA[row * MATRIX_WIDTH + i] * matrixB[i * MATRIX_WIDTH + col];
-      }
-    }
-  }
-}
+#define MATRIX_HEIGTH 128 * 64
+#define MATRIX_WIDTH 128 * 64
 
 int main()
 {
-  system_clock::time_point begin, end;
   int64_t general_time;
+  TIFF *bands_resampled[8];
+  system_clock::time_point begin, end;
 
-  // Create a matrix
-  float *matrixA = new float[MATRIX_HEIGTH * MATRIX_WIDTH];
-  float *matrixB = new float[MATRIX_HEIGTH * MATRIX_WIDTH];
-  float *matrixC = new float[MATRIX_HEIGTH * MATRIX_WIDTH];
+  // TIFFs Setup
+  std::string bands_paths[] = {
+      "",
+      "./input/scenes/B2.tif",
+      "./input/scenes/B3.tif",
+      "./input/scenes/B4.tif",
+      "./input/scenes/B5.tif",
+      "./input/scenes/B6.tif",
+      "./input/scenes/B10.tif",
+      "./input/scenes/B7.tif",
+      "./input/scenes/final_tal.tif"};
 
-  // Initialize the matrix
-  for (int i = 0; i < MATRIX_HEIGTH * MATRIX_WIDTH; i++)
+  for (int i = 1; i <= 8; i++)
   {
-    matrixA[i] = 4.0f;
-    matrixB[i] = 2.0f;
+    std::string path_tiff_base = bands_paths[i];
+    bands_resampled[i] = TIFFOpen(path_tiff_base.c_str(), "rm");
   }
 
-  // ======== RUN SERIAL =========
+  uint16_t sample_format;
+  uint32_t height, width;
+  TIFFGetField(bands_resampled[1], TIFFTAG_IMAGELENGTH, &height);
+  TIFFGetField(bands_resampled[1], TIFFTAG_IMAGEWIDTH, &width);
+  TIFFGetField(bands_resampled[1], TIFFTAG_SAMPLEFORMAT, &sample_format);
+
+  // Read TIFFs
+  double *band1 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band2 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band3 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band4 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band5 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band6 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band7 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+  double *band8 = new double[MATRIX_HEIGTH * MATRIX_WIDTH];
+
+  std::vector<double *> bands;
+  bands.push_back(band1);
+  bands.push_back(band2);
+  bands.push_back(band3);
+  bands.push_back(band4);
+  bands.push_back(band5);
+  bands.push_back(band6);
+  bands.push_back(band7);
+  bands.push_back(band8);
+
+  for (int i = 1; i <= 8; i++)
+  {
+    for (int line = 0; line < height; line++)
+    {
+      TIFF *curr_band = bands_resampled[i];
+      tdata_t band_line_buff = _TIFFmalloc(TIFFScanlineSize(curr_band));
+      unsigned short curr_band_line_size = TIFFScanlineSize(curr_band) / width;
+      TIFFReadScanline(curr_band, band_line_buff, line);
+
+      for (int col = 0; col < width; col++)
+      {
+        float value = 0;
+        memcpy(&value, static_cast<unsigned char *>(band_line_buff) + col * curr_band_line_size, curr_band_line_size);
+        bands[i - 1][line * width + col] = value;
+      }
+      _TIFFfree(band_line_buff);
+    }
+  }
+
+  // ======== RUN =========
+
   begin = system_clock::now();
 
-  // Make a scalar product
-  for (int row = 0; row < MATRIX_HEIGTH; row++)
-    for (int col = 0; col < MATRIX_WIDTH; col++)
-      for (int i = 0; i < MATRIX_WIDTH; i++)
-        matrixC[row * MATRIX_WIDTH + col] += matrixA[row * MATRIX_WIDTH + i] * matrixB[i * MATRIX_WIDTH + col];
-
-  // std::thread threads[8];
-  // int numThreads = 8;
-  // int rowsPerThread = MATRIX_HEIGTH / numThreads;
-
-  // for (int t = 0; t < numThreads; ++t)
-  // {
-  //   int startRow = t * rowsPerThread;
-  //   int endRow = (t == numThreads - 1) ? MATRIX_HEIGTH : startRow + rowsPerThread;
-  //   threads[t] = std::thread(multiplyPartial, matrixA, matrixB, matrixC, startRow, endRow);
-  // }
-
-  // for (int t = 0; t < numThreads; ++t)
-  // {
-  //   threads[t].join();
-  // }
+  for (int i = 0; i < 1; i++)
+  {
+    for (int j = 0; j < MATRIX_WIDTH; j++)
+    {
+      std::cout << bands[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
 
   end = system_clock::now();
   general_time = duration_cast<nanoseconds>(end.time_since_epoch() - begin.time_since_epoch()).count();
   std::cout << "CUDA CORE," << MATRIX_HEIGTH << " x " << MATRIX_WIDTH << ", " << general_time << std::endl;
-  // ======== RUN SERIAL =========
-
-  // Print the result
-  // std::cout << "Matrix C: " << std::endl;
-  // for (int i = 0; i < MATRIX_HEIGTH; i++)
-  // {
-  //   for (int j = 0; j < MATRIX_WIDTH; j++)
-  //   {
-  //     std::cout << matrixC[i * MATRIX_WIDTH + j] << " ";
-  //   }
-  //   std::cout << std::endl;
-  // }
-
-  // Free the memory
-  delete[] matrixA;
-  delete[] matrixB;
-  delete[] matrixC;
+  // ======== RUN =========
 
   return 0;
 }
